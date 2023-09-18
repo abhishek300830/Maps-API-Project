3906046287cc399bc6e912c42551e2e6bf1f9ab1
abhishek300830 <abhishek300830@gmail.com>
2023-09-18 10:45:05 +0530

    added text search

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
-from flask import Flask,request
+import requests
+
+from flask import Flask
 from flask_smorest import abort
+
+from env_varaibles import url,headers
+
 
 app = Flask(__name__)
 
-@app.get("/hello")
-def print_hello():
-    return {"message":"hello..done"}
+@app.get("/status")
+def check_status():
+    return {"status":"UP"}
+
+
+@app.get("/search/<string:query>")
+def query_serch(query):
+    querystring = {"radius":"1500","query":query}
+    response = requests.get(url+"/textsearch/json", headers=headers, params=querystring)
+
+    return {"ok":response.json()}
+
+    
---
+++ env_varaibles.py
@@ -0,0 +1,6 @@
+url = "https://map-places.p.rapidapi.com"
+
+headers = {
+        "X-RapidAPI-Key": "008991c9bfmsh9e5d124ab5c117ep1d9a01jsnf0ccb2ec40b8",
+        "X-RapidAPI-Host": "map-places.p.rapidapi.com"
+    }
