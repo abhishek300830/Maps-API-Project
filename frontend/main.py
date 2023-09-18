d3e1e8e768ac3089050b11db480be4aef2be0531
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-09-18 13:53:46 +0530

    Logging Changes

--- frontend/main.py
+++ frontend/main.py
@@ -1,5 +1,8 @@
 from helpers.entry_menu import EntryMenu
-
+import logging
+logging.basicConfig(filename='app.log', filemode='w',
+                    format='%(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def main():
     instance = EntryMenu()
