0f1b68b6caddc12531b1bf11f91b2e1598799928
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-09-18 10:57:30 +0530

    place details complete

--- app.py
+++ app.py
@@ -3,32 +3,39 @@ import requests
 from flask import Flask
 from flask_smorest import abort
 
-from env_varaibles import url,headers
+from env_varaibles import url, headers
 
 
 app = Flask(__name__)
 
+
 @app.get("/status")
 def check_status():
-    return {"status":"UP"}
+    return {"status": "UP"}
 
 
 @app.get("/search/<string:query>")
 def query_serch(query):
-    querystring = {"radius":"1500","query":query}
-    response = requests.get(url+"/textsearch/json", headers=headers, params=querystring)
+    querystring = {"radius": "1500", "query": query}
+    response = requests.get(url+"/textsearch/json",
+                            headers=headers, params=querystring)
 
-    return {"ok":response.json()}
+    return {"ok": response.json()}
 
 
 @app.get("/place/<string:query>")
 def place_autocomplete(query):
-    querystring = {"input":query,"radius":"50000"}
-    response = requests.get(url+"/autocomplete/json", headers=headers, params=querystring)
+    querystring = {"input": query, "radius": "50000"}
+    response = requests.get(url+"/autocomplete/json",
+                            headers=headers, params=querystring)
 
     return response.json()
 
 
+@app.get("/place/<string:placeid>/details")
+def place_details(placeid):
+    querystring = {"place_id": placeid}
+    response = requests.get(url+"/details/json",
+                            headers=headers, params=querystring)
 
-
-    
+    return response.json()
