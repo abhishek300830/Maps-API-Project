e16c2763753581f6e4b83aac1d59f5f954132ad8
abhishek300830 <abhishek300830@gmail.com>
2023-09-18 16:10:10 +0530

    entry menu

--- frontend/helpers/entry_menu.py
+++ frontend/helpers/entry_menu.py
@@ -1,7 +1,8 @@
-from typing import Any
 import inquirer
+
 from pprint import pprint
 from controllers.location import Location
+from controllers.places import Place
 
 
 class EntryMenu:
@@ -10,44 +11,50 @@ class EntryMenu:
 
         answer = self.taking_user_input()
 
-        print(answer)
-
         if answer.get('choice') == 'Search By Query':
             self.search_places_by_query()
+
         elif answer.get('choice') == 'Search By Location':
             self.search_places_by_location()
+
 
     def taking_user_input(self):
         print("This is Welcome Menu...")
         questions = [
             inquirer.List('choice',
                           message="Please Select Your Choice : ",
-                          choices=['Search By Query', 'Search By Location'],
+                          choices=['Search By Any Query', 'Search By Location'],
                           ),
         ]
         answer = inquirer.prompt(questions)
         return answer
 
     def search_places_by_location(self):
-        response = self.get_places_by_location()
+        location_instance = Location()
+        user_location = self.__input_user_location()
+        response = location_instance.get_location_by_query(user_location.get('query'))
+        
         pprint(response)
 
-    def get_places_by_location(self):
-        location_instance = Location()
-        response = location_instance.get_location_by_coordinates()
-        return response
+
+
+    def __input_user_location(self):
+        questions = [
+            inquirer.Text('query', message="Enter Your Location Name : ")
+        ]
+        answer = inquirer.prompt(questions)
+        return answer
+
+        
 
     def search_places_by_query(self):
-        response = self.get_places_by_query()
+        query = self.__input_user_query()
+        place_instance = Place()
+        response = place_instance.get_places_by_query(query.get('query'))
         pprint(response)
 
-    def get_places_by_query(self):
-        query = self.input_user_query()
-        location_instance = Location()
-        response = location_instance.get_location_by_query(query.get('query'))
-        return response
 
-    def input_user_query(self):
+    def __input_user_query(self):
         questions = [
             inquirer.Text('query', message="Enter Your Query : ")
         ]
@@ -55,7 +62,7 @@ class EntryMenu:
         return answer
 
 
-if __name__ == "__main__":
-    obj = EntryMenu()
-    res = obj.input_user_query()
-    print(res)
+# if __name__ == "__main__":
+#     obj = EntryMenu()
+#     res = obj.input_user_query()
+#     print(res)
