feaaa3a0905e51ef7251e077ffc18de3135eb1ff
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-09-18 12:00:42 +0530

    final base server end completed

--- app.py
+++ app.py
@@ -24,13 +24,14 @@ def query_serch(query):
 
     try:
         querystring = {"radius": "1500", "query": query}
-        response = requests.get(url+"/textsearch/json",headers=headers, params=querystring)
+        response = requests.get(url+"/textsearch/json",
+                                headers=headers, params=querystring)
 
         return {"ok": response.json()}
-        
+
     except Exception as error:
         logger.error(error)
-        abort(400,message="Bad Request")
+        abort(400, message="Bad Request")
 
 
 @app.get("/nearby_search/<string:location>")
@@ -38,13 +39,14 @@ def nearby_search(location):
     logger.debug(f"nearby search callled with params: {location}")
 
     try:
-        querystring = {"location":location,"radius":"1500"}
-        response = requests.get(url+"/nearbysearch/json", headers=headers, params=querystring)
+        querystring = {"location": location, "radius": "1500"}
+        response = requests.get(url+"/nearbysearch/json",
+                                headers=headers, params=querystring)
         return response.json()
-    
+
     except Exception as error:
         logger.error(error)
-        abort(400,message="Bad Request")
+        abort(400, message="Bad Request")
 
 
 @app.get("/place/<string:query>")
@@ -54,10 +56,11 @@ def place_autocomplete(query):
     try:
         response = requests.get(url+"/autocomplete/json",
                                 headers=headers, params=querystring)
+        return response.json()
+
     except Exception as error:
         logger.warning(error)
         abort(400, message="Bad Request")
-    return response.json()
 
 
 @app.get("/place/<string:placeid>/details")
@@ -67,9 +70,8 @@ def place_details(placeid):
     try:
         response = requests.get(url+"/details/json",
                                 headers=headers, params=querystring)
+        return response.json()
 
     except Exception as error:
         logger.warning(error)
         abort(400, message="Bad Request")
-
-    return response.json()
