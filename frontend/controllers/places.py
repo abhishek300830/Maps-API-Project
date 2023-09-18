31a5fd80e65b43d292c001ad11cf59646c20b0e9
Abhijeet Singh Hada <abhi22hada@gmail.com>
2023-09-18 14:53:31 +0530

    places class with get_places by location and query

--- frontend/controllers/places.py
+++ frontend/controllers/places.py
@@ -1,17 +1,37 @@
+from helpers.env_variables import base_url
 import requests
-from helpers.env_variables import url, headers
-from flask_smorest import abort
+import requests.exceptions
+import logging
+logger = logging.getLogger(__name__)
 
 
 class Place:
 
-    def get_places_by_location(self, location):
-        querystring = {"location": location, "radius": "1500"}
+    def get_places_by_location(self, location):  # location = "lat,lng"
+        logger.debug(f"called get_places_by_location with params: {location}")
         try:
-            response = requests.get(url+"/nearbysearch/json",
-                                    headers=headers, params=querystring)
+            response = requests.get(
+                base_url+f"/nearby_search/{location}")
             return response.json()
 
-        except Exception as error:
-            logger.error(error)
-            abort(400, message="Bad Request")
+        except requests.exceptions as error:
+            logger.error(f"Error in get_places_by_location: {error}")
+            return {"error": "Something went wrong while fetching places."}
+
+    def get_places_by_query(self, query):
+        logger.debug(f"called get_places_by_query with params: {query}")
+        try:
+            response = requests.get(
+                base_url+f"/text_search/{query}")
+            return response.json()
+
+        except requests.exceptions as error:
+            logger.error(f"Error in get_places_by_query: {error}")
+            return {"error": "Something went wrong while fetching places."}
+
+
+if __name__ == "__main__":
+    instance = Place()
+    response = instance.get_places_by_location("12.9716,77.5946")
+
+    print(response)
